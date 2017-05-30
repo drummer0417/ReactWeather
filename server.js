45408a52a0cec13cd121382739f90e1e1ec0d4f6
drummer0417 <hans@androidappfactory.nl>
2017-05-30 18:27:13 +0200

    Made it heroku proof

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ var express = require('express');
 
 // Create our app
 var app = express();
-var port = 3000;
+var port = process.env.port || 3000;
+
+app.use(function(req, res, next) {
+  if (!req.headers['x-forwarded-proto'] || req.headers['x-forwarded-proto'] === 'http') {
+
+    console.log('in is http');
+    next();
+  } else {
+    console.log('in not is http but https');
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
