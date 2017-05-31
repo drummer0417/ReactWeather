744bebb65824907ddf89110be41165c5c97ab408
drummer0417 <hans@androidappfactory.nl>
2017-05-31 13:08:33 +0200

    Added some style to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var { Link } = require('react-router');
 
 // var About = React.createClass({
 //   render: function(){
@@ -15,17 +16,22 @@ var About = (props) => {
   return (
     <div>
         <h1 className="text-center">About</h1>
-      <p>This is version 0.1 of HansWeatherApp<br/><br/>
-        Can't wait to add some style to make it look less terrible
-      </p>
-      <p>You are on a small screen or larger.</p>
-      <p className="show-for-medium">You are on a medium screen or larger.</p>
-      <p className="show-for-large">You are on a large screen or larger.</p>
-      <p className="show-for-small-only">You are <em>definitely</em> on a small screen.</p>
-      <p className="show-for-medium-only">You are <em>definitely</em> on a medium screen.</p>
-      <p className="show-for-large-only">You are <em>definitely</em> on a large screen.</p>
-      <p className="hide">Can't touch this.</p>
-      <p className="invisible">Can sort of touch this.</p>
+      <p>This is version 0.1 of HansWeatherApp<br/><br/></p>
+      <p>The app is one of the apps created in the course "<a href='https://www.udemy.com/courses/' target='_blank'>The Complete React Web App Developer</a>"
+       </p>
+       <p>
+         Some of the tools I used for building this app
+       </p>
+       <p>
+         <ul>
+           <li><a href="https://facebook.github.io/react" taret="_blank">React</a> - as a javascript framework</li>
+           <li><a href="http://openweathermap.org/" taret="_blank">Open Weather Map</a> - the weather api used</li>
+           <li>Webpac to bundle the application into a javascript file</li>
+           <li>Foundation as a css framework</li>
+           <li>jQuery</li>
+         </ul>
+       </p>
+
     </div>
   )
 }
