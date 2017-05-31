7bc97285c9aa9eb8fcb80f998645f2c47b768845
drummer0417 <hans@androidappfactory.nl>
2017-05-31 10:45:18 +0200

    Added some more style

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -19,8 +19,13 @@ var About = (props) => {
         Can't wait to add some style to make it look less terrible
       </p>
       <p>You are on a small screen or larger.</p>
-      <p class="show-for-medium">You are on a medium screen or larger.</p>
-      <p class="show-for-large">You are on a large screen or larger.</p>
+      <p className="show-for-medium">You are on a medium screen or larger.</p>
+      <p className="show-for-large">You are on a large screen or larger.</p>
+      <p className="show-for-small-only">You are <em>definitely</em> on a small screen.</p>
+      <p className="show-for-medium-only">You are <em>definitely</em> on a medium screen.</p>
+      <p className="show-for-large-only">You are <em>definitely</em> on a large screen.</p>
+      <p className="hide">Can't touch this.</p>
+      <p className="invisible">Can sort of touch this.</p>
     </div>
   )
 }
